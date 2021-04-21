c9d3bbddc55d5745d8438dc0f111413185629917
Yesid Hoyos <yesidfernandohoyos@gmail.com>
2021-04-21 16:49:21 -0500

    initial commit

---
+++ src/test/java/com/woloxgram/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woloxgram.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
